84bfb5196f15b3b5d55234a9bbb1af2145e90b3b
Matthieu <matthieunadeau96@gmail.com>
2020-04-14 14:51:10 -0400

    Created Appbar and floating action widget & added Presently styling and fonts

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        fontFamily: 'LibreBaskerville',
+        primaryColor: Color(0xffDBD1C7),
+        accentColor: Colors.black,
+        primaryTextTheme: TextTheme(
+          title: TextStyle(color: Theme.of(context).primaryColor),
+        ),
       ),
       home: MyHomePage(),
     );
@@ -24,9 +29,44 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Presently Clone'),
+        brightness: Brightness.dark,
+        iconTheme: IconThemeData(
+          color: Theme.of(context).primaryColor,
+        ),
+        backgroundColor: Theme.of(context).accentColor,
+        leading: Icon(
+          Icons.search,
+          color: Theme.of(context).primaryColor,
+          size: 30,
+        ),
+        title: Text(
+          'Presently',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Theme.of(context).primaryColor,
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+            color: Theme.of(context).primaryColor,
+          )
+        ],
       ),
-      body: Center(child: Text('Hello')),
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Text('Hello'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(
+          Icons.calendar_today,
+          color: Theme.of(context).primaryColor,
+        ),
+      ),
     );
   }
 }
