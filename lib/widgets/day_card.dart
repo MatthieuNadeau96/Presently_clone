6e6b22cab3be76737b48b6f5a0bac2c41d183985
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-04-16 15:59:21 -0400

    added a simple navigator

--- lib/widgets/day_card.dart
+++ lib/widgets/day_card.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:presently_clone/screens/edit_screen.dart';
 
 class DayCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 0,
-      color: Theme.of(context).primaryColor,
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              'April 15 2020',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => EditScreen(),
+          ),
+        );
+      },
+      child: Card(
+        elevation: 0,
+        color: Theme.of(context).primaryColor,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                'April 15 2020',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 6),
-            Text(
-              'What are you grateful for today?',
-              style: TextStyle(
-                color: Color(0xff89827C),
+              SizedBox(height: 6),
+              Text(
+                'What are you grateful for today?',
+                style: TextStyle(
+                  color: Color(0xff89827C),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
