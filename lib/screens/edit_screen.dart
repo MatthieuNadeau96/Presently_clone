43d6190c46eaa0b7b7485eecb40a1ae404ce27eb
Matthieu Nadeau <matthieunadeau96@gmail.com>
2020-04-18 10:13:09 -0400

    Added more ui elements to the edit_screen & created a more dynamic way of sizing the sections

--- lib/screens/edit_screen.dart
+++ lib/screens/edit_screen.dart
@@ -4,84 +4,129 @@ class EditScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Theme.of(context).primaryColor,
-        body: Padding(
-          padding: const EdgeInsets.symmetric(
-            vertical: 60,
-            horizontal: 30,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                child: Text(
-                  'Today',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Container(
-                child: Text(
-                  'I am grateful for',
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              Divider(
-                height: 0,
-                thickness: 3,
-                color: Colors.black,
-              ),
-              Container(
-                height: MediaQuery.of(context).size.height * 0.55,
-                child: TextField(
-                  textAlign: TextAlign.start,
-                  decoration: InputDecoration(
-                    labelText: 'What are you grateful for?',
-                    labelStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                    border: InputBorder.none,
-                    focusedBorder: InputBorder.none,
-                    enabledBorder: InputBorder.none,
-                    errorBorder: InputBorder.none,
-                    disabledBorder: InputBorder.none,
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Padding(
+        padding: EdgeInsets.only(
+          top: MediaQuery.of(context).size.height * 0.10,
+          left: 30,
+          right: 30,
+          bottom: 0,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height * 0.15,
+              child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
-                  GestureDetector(
-                    child: Icon(
-                      Icons.lightbulb_outline,
-                      size: 30,
-                    ),
-                    onTap: () {},
-                  ),
-                  FlatButton(
-                    onPressed: () {},
+                  Container(
                     child: Text(
-                      'SAVE',
-                      style: TextStyle(color: Theme.of(context).primaryColor),
+                      'Today',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                    color: Colors.black,
+                  ),
+                  Container(
+                    child: Text(
+                      'I am grateful for',
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ],
               ),
-              Divider(
+            ), // TITLE SECTION
+            Divider(
+              height: 0,
+              thickness: 3,
+              color: Colors.black,
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.55,
+                    child: TextField(
+                      textAlign: TextAlign.start,
+                      decoration: InputDecoration(
+                        labelText: 'What are you grateful for?',
+                        labelStyle: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                        border: InputBorder.none,
+                        focusedBorder: InputBorder.none,
+                        enabledBorder: InputBorder.none,
+                        errorBorder: InputBorder.none,
+                        disabledBorder: InputBorder.none,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        GestureDetector(
+                          child: Icon(
+                            Icons.lightbulb_outline,
+                            size: 30,
+                          ),
+                          onTap: () {},
+                        ),
+                        RaisedButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(2.5),
+                          ),
+                          onPressed: () {},
+                          child: Text(
+                            'SAVE',
+                            style: TextStyle(
+                                color: Theme.of(context).primaryColor),
+                          ),
+                          color: Colors.black,
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.02,
+                bottom: 0,
+              ),
+              child: Divider(
                 height: 0,
                 thickness: 3,
                 color: Colors.black,
               ),
-            ],
-          ),
-        ));
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.13,
+              width: double.infinity,
+              child: Center(
+                child: Text(
+                  '\"The root of joy is gratefulness\" \n David Steindl-Rast',
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+// WTF did you do dood
+// i think ill know
+// tried to make the colored section mediaqueries
+// fix that
